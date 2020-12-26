9c79bce4e63c983e6ab11d83fad270aeddb3c2e1
Jelle Besseling <jelle@pingiun.com>
2020-12-26 15:08:28 +0100

    Use lower check interval because webhooks are set up

--- fred.thalia.nu/repo.nix
+++ fred.thalia.nu/repo.nix
@@ -6,7 +6,7 @@ repo: {
   nixexprinput = "local";
   nixexprpath = "jobsets.nix";
 
-  checkinterval = "20";
+  checkinterval = "120";
   schedulingshares = 1;
   enableemail = false;
   emailoverride = "";
