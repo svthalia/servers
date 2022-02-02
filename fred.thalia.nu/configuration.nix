c704d4d02ff437907371406604df2511b9d48260
Jelle Besseling <jelle@pingiun.com>
2022-02-02 19:55:54 +0100

    Use stable nix

--- fred.thalia.nu/configuration.nix
+++ fred.thalia.nu/configuration.nix
@@ -67,7 +67,7 @@ in
     experimental-features = nix-command flakes
   '';
   # this is required until nix 2.4 is released
-  nix.package = pkgs.nixUnstable;
+  # nix.package = pkgs.nixUnstable;
 
   environment.systemPackages = with pkgs; [
     git
